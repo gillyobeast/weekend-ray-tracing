1666adc823e1a63650f1699b53da7d9745f8c0cf
Luke Armitage <gillyobeast@gmail.com>
2023-10-20 21:58:19 +0100

    write to file

--- src/main/kotlin/RayTracer.kt
+++ src/main/kotlin/RayTracer.kt
@@ -1,19 +1,27 @@
+import java.io.File
+
 fun main() {
+    val file = File("./output.ppm")
     val width = 200
     val height = 100
-    println("P3")
-    println("$width $height")
-    println("255")
+    file.writeText("P3\n")
+    file.write("$width $height")
+    file.write("255")
 
     for (row in height.downTo(0)) {
         for (column in 0..width) {
             val red = column.toDouble() / width
             val green = row.toDouble() / width
             val blue = 0.2
-            println("${red.scale()} ${green.scale()} ${blue.scale()}")
+            file.write("${red.scale()} ${green.scale()} ${blue.scale()}")
         }
     }
 }
+
+private fun File.write(s: String) {
+    appendText(s + "\n")
+}
+
 
 private fun Double.scale() = (this * 255.99).toInt()
 
