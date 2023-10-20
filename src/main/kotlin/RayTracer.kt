b8235bc9b14ecc3932b37eaee40cd913d337df08
Lake Armitage <gillyobeast@gmail.com>
2023-10-21 00:36:34 +0100

    chapter 4: WIP: a sphere

--- src/main/kotlin/RayTracer.kt
+++ src/main/kotlin/RayTracer.kt
@@ -2,17 +2,17 @@ import java.io.File
 
 fun main() {
     val file = File("./output.ppm")
-    val width = 200
-    val height = 100
+    val width = 20
+    val height = 10
     file.writePpmHeader(width, height)
     val lowerLeft = Point(-2, -1, -1)
     val horizontal = Vec3(4, 0, 0)
     val vertical = Vec3(0, 2, 0)
     val origin = Point(0, 0, 0)
-    for (row in (height - 1).downTo(0)) {
+    for (row in (height - 1)downTo 0) {
         for (column in 0..<width) {
-            val u:Double = column.toDouble() / width.toDouble()
-            val v:Double = row.toDouble() / width.toDouble()
+            val u: Double = column.toDouble() / width.toDouble()
+            val v: Double = row.toDouble() / width.toDouble()
             val ray = Ray(origin, lowerLeft + u * horizontal + v * vertical)
             val colour = colour(ray)
             file.write("${colour.r.scale()} ${colour.g.scale()} ${colour.b.scale()}")
@@ -20,7 +20,19 @@ fun main() {
     }
 }
 
+fun Ray.hitsSphere(center: Point, radius: Double): Boolean {
+    val oc = origin - center
+    val a = direction dot direction
+    val b = 2.0 * oc dot direction
+    val c = (oc dot oc) - (radius * radius)
+    val discriminant = (b * b) - (4 * a * c);
+    return discriminant > 0
+}
+
 fun colour(ray: Ray): Colour {
+    if (ray.hitsSphere(Point(0, 0, -1), 0.5)){
+        return Colour(1,0,0)
+    }
     val unitDir = ray.direction.makeUnitVector()
     val t = 0.5 * (unitDir.y + 1.0)
     return (1.0 - t) * Colour(1, 1, 1) + t * Colour(0.7, 0.2, 1.0)
